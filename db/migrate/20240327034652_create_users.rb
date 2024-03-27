49079d60d8dfc2c8a2ccdce092ff965be9adaf35
matthewdillender <156735514+matthewdillender@users.noreply.github.com>
2024-03-26 20:48:14 -0700

    add usercontroller

--- db/migrate/20240327034652_create_users.rb
+++ db/migrate/20240327034652_create_users.rb
@@ -2,9 +2,8 @@ class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :password
       t.string :email
-      t.string :password_confirmation
+      t.string :password_digest
 
       t.timestamps
     end
